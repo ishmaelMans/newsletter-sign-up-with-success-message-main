2a6d1bb206761e1babe89ece52929b0ab7775989
ishmaelMans <mansarayishmael17@gmail.com>
2025-01-01 01:57:54 -0600

    v1

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-let isClicked = false;
 const button = document.getElementById("subscribe");
 const thanksForSub = document.getElementById("thanks-for-sub");
 const email = document.getElementById("email");
-const dismiss = document.getElementById("dismiss");
+
+let isClicked = false;
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  if (!isClicked) {
+  if (email.value) {
+    container = document.createElement("div");
+    container.innerHTML = ` 
+              <img src="./assets/images/icon-list.svg" alt="" />
+              <h1>Thanks for subscribing!</h1>
+              <p>
+                A confirmation email has been sent to
+                <span>${email.value}.</span> Please open it and click the
+                button inside to confirm your subscription.
+              </p>
+              <button type="button" id="dismiss" onclick="handleDismiss()" >Dismiss message</button>
+            </div>`;
+    thanksForSub.appendChild(container);
     thanksForSub.classList.add("show");
-    console.log(email.value);
   }
 });
 
-dismiss.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (!isClicked) {
-    thanksForSub.classList.remove("show");
-  }
-});
+const dismiss = document.getElementById("dismiss");
+
+function handleDismiss() {
+  container.innerHTML = "";
+  thanksForSub.innerHTML = "";
+  thanksForSub.classList.remove("show");
+  isClicked = false;
+  email.value = "";
+  console.log("dismiss");
+}
